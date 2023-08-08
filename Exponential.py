14d501fa4f988bf90609a679b3a6a54f8c4bfd32
222010301028-Dennis <222010301028.dennis@gmail.com>
2023-08-08 11:37:10 +0530

    Update 2

--- Exponential.py
+++ Exponential.py
@@ -2,7 +2,7 @@ a = int(input("Enter value for A (between 1 and 10): "))
 b = int(input("Enter value for B (between 1 and 5): "))
 
 def Exponential_range(a,b):
-    if a>11 and b>5:
+    if a>10 and b>5:
         print("Both NOT in range")
     else:
         if a not in range(11):
